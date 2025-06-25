45133edda5b0835737a448d7ef8745b13fed30e3
JaideepYadav <jaideepyadav611@gmail.com>
2025-06-25 23:38:51 +0530

    A blogging webiste
    Blogging website using flask and python

---
+++ Controllers/post_controllers.py
@@ -0,0 +1,15 @@
+from models.post import Post
+from flask import flash, redirect, url_for
+
+def list_posts(published):
+    """
+    TODO: Query Post model filtered by is_published status.
+    Return a list of posts.
+    """
+    
+    
+def get_post(post_id):
+    """
+    TODO: Retrieve a post by its ID or abort with 404 if not found.
+    """
+    pass
