620d1deb9ad8e4d3a48f159c168180564544407a
JaideepYadav <jaideepyadav611@gmail.com>
2025-06-29 22:58:29 +0530

    Updated Base file

--- forms/post_form.py
+++ forms/post_form.py
@@ -1,9 +1,14 @@
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, BooleanField
+from wtforms import PasswordField, StringField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired, Length, Email
-class PostForm(FlaskForm):
-    title = StringField("Title", validators=[DataRequired(), Length(min=4, max=100)])
-    body = TextAreaField("Body", validators=[DataRequired(), Length(min=10, max=500)])
-    author = StringField("Author", validators=[DataRequired(), Length(min=2, max=25)])
+class Signup(FlaskForm):
+    user = StringField("User", validators=[DataRequired(), Length(min=4, max=100)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=2, max=25)])
     email = StringField("Email", validators=[Email(message="Invalid email")])
+class Login(FlaskForm):
+    email=StringField("Email", validators=[Email(message="Invalid email")])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=2, max=25)])
+
+
+    
